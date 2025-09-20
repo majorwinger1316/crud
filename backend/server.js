a4aa17db4a98f14a47bd8c8a9ce9af3cebf36f19
majorwinger1316 <ak4425@srmist.edu.in>
2025-09-21 01:07:26 +0530

    testing

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.post("/create", (req, res) => {
     if (err) {
       console.error("Error adding animal:", err);
       return res
-        .status(1000)
+        .status(900)
         .json({ error: "An error occurred while adding the animal." });
     }
     return res.status(201).json({ message: "Animal added successfully" });
